662c37480c85dd99be206888277dbf398314e84e
ayzar <valayzar@gmail.com>
2024-11-19 06:21:45 +0300

    Анализатор прайс-листов. Правка html

--- project.py
+++ project.py
@@ -70,10 +70,13 @@ class PriceMachine():
         return product_idx, price_idx, weight_idx
 
     def export_to_html(self, fname='output.html'):
-        result = '''
+        result = '''        
         <!DOCTYPE html>
-        <html>
+    <html lang="ru">
         <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Позиции продуктов</title>
         </head>
         <body>
